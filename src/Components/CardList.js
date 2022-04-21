f8e66fc7fa905c53815fcd03de23bc41b8ea5432
Olorunyomi69 <isaacolorunyomi69@gmail.com>
2022-04-21 16:07:04 +0100

    Updated the scroll and organized the folders

--- src/Components/CardList.js
+++ src/Components/CardList.js
@@ -6,7 +6,8 @@ const cardList = ({robots}) => {
            {
                robots.map((user, i) =>{
          return (
-                 <Card key= {i}
+                 <Card 
+                key= {i}
                 id= { robots[i].id }
                 name= {robots[i].name}
                 email= {robots[i].email}
